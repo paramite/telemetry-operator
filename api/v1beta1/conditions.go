dac255809e4bc5162dd0cdd4f7890cd345dff6f8
Martin Mágr <mmagr@redhat.com>
2024-03-01 15:32:54 +0100

    Add Availabilty resource
    This CRD deploys kube-state-metrics and respective ScrapeConfig to gather
    pod and container status metrics.

--- api/v1beta1/conditions.go
+++ api/v1beta1/conditions.go
@@ -61,6 +61,9 @@ const (
 	DashboardDatasourceReadyCondition condition.Type = "DashboardDatasourceReady"
 
 	DashboardDefinitionReadyCondition condition.Type = "DashboardDefinitionReady"
+
+	// AvailabilityReadyCondition Status=True condition which indicates if the Availability is configured and operational
+	AvailabilityReadyCondition condition.Type = "AvailabilityReady"
 )
 
 // Telemetry Reasons used by API objects.
@@ -202,15 +205,30 @@ const (
 
 	DashboardsNotEnabledMessage = "Dashboarding was not enabled, so no actions required"
 
-	DashboardPrometheusRuleReadyInitMessage = "Dashboard PrometheusRule not started"
+	DashboardPrometheusRuleReadyInitMessage   = "Dashboard PrometheusRule not started"
 	DashboardPrometheusRuleUnableToOwnMessage = "Error occured when trying to own %s"
 
 	DashboardPluginReadyInitMessage = "Dashboard Plugin not started"
 	DashboardPluginFailedMessage = "Error occured when trying to install the dashboard plugin: %s"
 
 	DashboardDatasourceReadyInitMessage = "Dashboard Datasource not started"
-	DashboardDatasourceFailedMessage = "Error occured when trying to install the dashboard datasource: %s"
+	DashboardDatasourceFailedMessage    = "Error occured when trying to install the dashboard datasource: %s"
 
 	DashboardDefinitionReadyInitMessage = "Dashboard Definition not started"
-	DashboardDefinitionFailedMessage = "Error occured when trying to install the dashboard definitions: %s"
+	DashboardDefinitionFailedMessage    = "Error occured when trying to install the dashboard definitions: %s"
+
+	//
+	// AvailabilityReady condition messages
+	//
+	// AvailabilityReadyInitMessage
+	AvailabilityReadyInitMessage = "Availability not started"
+
+	// AvailabilityReadyMessage
+	AvailabilityReadyMessage = "Availability completed"
+
+	// AvailabilityReadyErrorMessage
+	AvailabilityReadyErrorMessage = "Availability error occured %s"
+
+	// AvailabilityReadyRunningMessage
+	AvailabilityReadyRunningMessage = "Availability in progress"
 )
